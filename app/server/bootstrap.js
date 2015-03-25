e0f17327abf74c0fa9ce082f83fd1a7bc7a8962a
SungpilNam <iimplicit@gmail.com>
2015-03-26 01:53:13 +0900

    Facebook 로그인 추가 및 home 페이지 input 추가 및 일부 html 구현.

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,13 @@
 Meteor.startup(function () {
 });
+
+
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1584785935107257',
+    secret: '9ce743736d7d701bda325e3849427cb6'
+});
