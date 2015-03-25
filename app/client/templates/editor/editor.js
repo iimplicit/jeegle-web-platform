aede9c353f55b6cb2dec60efacb67157710f5878
SungpilNam <iimplicit@gmail.com>
2015-03-26 01:53:57 +0900

    draggable, resizable, ckeditor 구현.

--- app/client/templates/editor/editor.js
+++ app/client/templates/editor/editor.js
@@ -24,8 +24,7 @@ Template.Editor.rendered = function() {
     //            invert: 0 + '%', // 0 ~ 100
     //            saturate: 100 + '%', // 0 ~ 500
     //            sepia: 0 + '%' // 0 ~ 100
-    function imageApp(){
-    }
+    function imageApp() {}
 
     imageApp.prototype = {
         targetImage: $('#main-target'),
@@ -177,6 +176,197 @@ Template.Editor.rendered = function() {
 
     var app = new imageApp();
     app.init();
+
+    // target elements with the "draggable" class
+    interact('.draggable')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent",
+                endOnly: true,
+                elementRect: {
+                    top: 0,
+                    left: 0,
+                    bottom: 1,
+                    right: 1
+                }
+            },
+
+            // call this function on every dragmove event
+            onmove: function(event) {
+                var target = event.target,
+                    // keep the dragged position in the data-x/data-y attributes
+                    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+                // translate the element
+                target.style.webkitTransform =
+                    target.style.transform =
+                    'translate(' + x + 'px, ' + y + 'px)';
+
+                // update the posiion attributes
+                target.setAttribute('data-x', x);
+                target.setAttribute('data-y', y);
+            },
+            // call this function on every dragend event
+            onend: function(event) {
+                var textEl = event.target.querySelector('p');
+
+                textEl && (textEl.textContent =
+                    'moved a distance of ' + (Math.sqrt(event.dx * event.dx +
+                        event.dy * event.dy) | 0) + 'px');
+            }
+        });
+
+    var offset = {
+        x: 0,
+        y: 0
+    };
+
+    interact('.draggable')
+        .resizable({
+            edges: {
+                left: true,
+                right: true,
+                bottom: true,
+                top: true
+            }
+        })
+        .on('resizemove', function(event) {
+            var target = event.target;
+
+            // update the element's style
+            target.style.width = event.rect.width + 'px';
+            target.style.height = event.rect.height + 'px';
+
+            // translate when resizing from top or left edges
+            offset.x += event.deltaRect.left;
+            offset.y += event.deltaRect.top;
+
+            target.style.transform = ('translate(' + offset.x + 'px,' + offset.y + 'px)');
+
+            target.textContent = event.rect.width + '×' + event.rect.height;
+        });
+
+    editorApp = {
+        data: {},
+
+        init: function() {
+            this.initCkeditor();
+            this.addEventListener();
+        },
+
+        addEventListener: function() {
+            this.setInlineEdiorDbclick();
+        },
+
+        initCkeditor: function() {
+            // This code is generally not necessary, but it is here to demonstrate
+            // how to customize specific editor instances on the fly. This fits well
+            // this demo because we have editable elements (like headers) that
+            // require less features.
+
+            // The "instanceCreated" event is fired for every editor instance created.
+            CKEDITOR.on('instanceCreated', function(event) {
+                var editor = event.editor,
+                    element = editor.element;
+
+                // Customize editors for headers and tag list.
+                // These editors don't need features like smileys, templates, iframes etc.
+                if (element.is('h1', 'h2', 'h3') || element.getAttribute('id') == 'taglist') {
+                    // Customize the editor configurations on "configLoaded" event,
+                    // which is fired after the configuration file loading and
+                    // execution. This makes it possible to change the
+                    // configurations before the editor initialization takes place.
+                    editor.on('configLoaded', function() {
+
+                        // Remove unnecessary plugins to make the editor simpler.
+                        editor.config.removePlugins = 'colorbutton,find,flash,font,' +
+                            'forms,iframe,image,newpage,removeformat,' +
+                            'smiley,specialchar,stylescombo,templates';
+
+                        // Rearrange the layout of the toolbar.
+                        editor.config.toolbarGroups = [{
+                            name: 'editing',
+                            groups: ['basicstyles', 'links']
+                        }, {
+                            name: 'undo'
+                        }, {
+                            name: 'clipboard',
+                            groups: ['selection', 'clipboard']
+                        }, {
+                            name: 'about'
+                        }];
+                    });
+                }
+            });
+        },
+
+        setInlineEdiorDbclick: function() {
+            var self = this;
+            $('.draggable').dblclick(function() {
+                // We need to turn off the automatic editor creation first.
+                var targetId = 'drag-me';
+                self.editorInit(targetId);
+            })
+        },
+
+        editorInit: function(targetId) {
+            CKEDITOR.disableAutoInline = true;
+
+            var activeEditor = 0;
+            var activeEditorElement = 0;
+            var activeId = '';
+
+            // Is there already an editor active?
+            if (activeEditor) {
+
+                // Is the active editor the same
+                // as the one being requested?
+                if (activeEditor.element.getId() == activeId) {
+
+                    // Then our editor is already running
+                    // so there's nothing to do!
+                    return;
+                }
+                activeEditor.destroy();
+            }
+
+            // Find the element that we want to
+            // attach the editor to
+            if (!(activeEditorElement = document.getElementById(targetId))) {
+                return;
+            }
+
+            // TODO - verify that the element is either a
+            // div or a textarea which are the only two
+            // element types supporting contenteditable=true
+
+            // Make the element editable
+            activeEditorElement.setAttribute('contenteditable', 'true');
+
+            // Create a new inline editor for this div
+            activeEditor = CKEDITOR.inline(targetId);
+
+            // Set up a destruction function that will occur
+            // when the user clicks out of the editable space
+            activeEditor.on('blur', function() {
+                this.element.setAttribute('contenteditable', 'false');
+                activeId = '';
+                activeEditor = 0;
+                activeEditorElement = 0;
+                this.destroy();
+            });
+
+            // Now set the focus to our editor so
+            // that it will open up for business
+            activeEditorElement.focus();
+        }
+    }
+
+    editorApp.init();
 };
 
 Template.Editor.destroyed = function() {};
