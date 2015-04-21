e0dbe3ff67929869186a799ffd2494011f0d1d38
extensioner <iimplicit@gmail.com>
2015-04-22 03:43:46 +0900

    사용자들 접속 시 모바일의 경우 비트 앱으로 가서 음악을 재생하게 하고, 웹의 경우 홈페이지로 이동할 수 있도록 하였습니다.

--- app/client/templates/music/music.js
+++ app/client/templates/music/music.js
@@ -7,8 +7,8 @@ Template.Music.events({
 		// 한편 created, rendered, destroyed 내에서 this는 Blaze.TemplateInstance입니다.
 		// 즉, events의 this와 후자의 this.data는 같은 객체를 가리킵니다.
 		console.log(this);
-		// var musicId = this.musicId;
-		// window.location = "bpc://landing?type=play_radio&channel_id=60&track_id=300000000000000000000000" + musicId;
+		var musicId = this.musicId;
+		window.location = "bpc://landing?type=play_radio&channel_id=61&track_id=300000000000000000000000" + musicId;
 	}
 });
 
